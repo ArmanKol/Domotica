fe948863d49df11220f0e76696666125d7c5d6dd
teunwiersma <31851921+teunwiersma@users.noreply.github.com>
2018-01-23 11:16:22 +0100

    gui versie 0.7

--- gui.py
+++ gui.py
@@ -34,7 +34,7 @@ def hoofdmenu():
     kamer1noodinformatie = tk.Label(master=hoofdmenuframe, background="royal blue", text="Noodinformatie: ")
     kamer1noodinformatie.grid(row=5, column=0, padx=5, pady=5)
 
-    kamer1noodknop = tk.Button(master=hoofdmenuframe, width=2, command=toonnoodmenuframe)
+    kamer1noodknop = tk.Button(master=hoofdmenuframe, width=2, command=toonnoodmenuframe1)
     kamer1noodknop.grid(row=5, column=1, padx=5, pady=5)
 
     #KAMER 2
@@ -68,77 +68,134 @@ def hoofdmenu():
     kamer2noodinformatie = tk.Label(master=hoofdmenuframe, background="royal blue", text="Noodinformatie: ")
     kamer2noodinformatie.grid(row=5, column=4, padx=5, pady=5)
 
-    kamer2noodknop = tk.Button(master=hoofdmenuframe, width=2)
+    kamer2noodknop = tk.Button(master=hoofdmenuframe, width=2, command=toonnoodmenuframe2)
     kamer2noodknop.grid(row=5, column=5, padx=5, pady=5)
 
     #Separater
     sep = Separator(hoofdmenuframe, orient="vertical")
     sep.grid(row=0, rowspan=6, column=3, sticky="ns")
 
-def noodvenster():
-    hoofdmenuterugknop = tk.Button(master=noodmenuframe, width=5, text="Terug", command=toonhoofdmenuframe)
+def noodvensterkamer1():
+    hoofdmenuterugknop = tk.Button(master=noodmenuframe1, width=5, text="Terug", command=toonhoofdmenuframe)
     hoofdmenuterugknop.grid(row=11, column=0)
 
-    noodvenstercamerabeeldentabel = tk.Label(master=noodmenuframe, background="royal blue",text="Camerabeelden: ")
+    noodvenstercamerabeeldentabel = tk.Label(master=noodmenuframe1, background="royal blue",text="Camerabeelden: ")
     noodvenstercamerabeeldentabel.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
 
-    noodvenstercameralicht = tk.Label(master=noodmenuframe, background="red", width=2)
+    noodvenstercameralicht = tk.Label(master=noodmenuframe1, background="red", width=2)
     noodvenstercameralicht.grid(row=0, column=1)
 
-    noodvenstercameraknop = tk.Button(master=noodmenuframe, width=2)
+    noodvenstercameraknop = tk.Button(master=noodmenuframe1, width=2)
     noodvenstercameraknop.grid(row=0, column=2, padx=5, pady=5)
 
-    noodvenstercontactgegevenslabel = tk.Label(master=noodmenuframe, background="royal blue", text="Noodcontactgegevens", font=("",15))
+    noodvenstercontactgegevenslabel = tk.Label(master=noodmenuframe1, background="royal blue", text="Noodcontactgegevens", font=("",15))
     noodvenstercontactgegevenslabel.grid(row=1, column=0, padx=5, pady=5)
 
-    noodvenstervoornaamlabel = tk.Label(master=noodmenuframe, background="royal blue", text="Voornaam: \t\t"+ databasereader("""SELECT voornaam from persoon where noodpersoonid = 1"""))
+    noodvenstervoornaamlabel = tk.Label(master=noodmenuframe1, background="royal blue", text="Voornaam: \t\t"+ databasereader("""SELECT voornaam from persoon where noodpersoonid = 1"""))
     noodvenstervoornaamlabel.grid(row=2, column=0, padx=5, pady=5, sticky=tk.W)
 
-    noodvenstertussenvoegsellabel = tk.Label(master=noodmenuframe, background="royal blue", text="Tussenvoegsel: \t\t" + str(databasereader("""SELECT tussenvoegsel from persoon where noodpersoonid = 1""")))
+    noodvenstertussenvoegsellabel = tk.Label(master=noodmenuframe1, background="royal blue", text="Tussenvoegsel: \t\t" + str(databasereader("""SELECT tussenvoegsel from persoon where noodpersoonid = 1""")))
     noodvenstertussenvoegsellabel.grid(row=3, column=0, padx=5, pady=5, sticky=tk.W)
 
-    noodvensterachternaamlabel = tk.Label(master=noodmenuframe, background="royal blue", text="Achternaam: \t\t"+ databasereader("""SELECT achternaam from persoon where noodpersoonid = 1"""))
+    noodvensterachternaamlabel = tk.Label(master=noodmenuframe1, background="royal blue", text="Achternaam: \t\t"+ databasereader("""SELECT achternaam from persoon where noodpersoonid = 1"""))
     noodvensterachternaamlabel.grid(row=4, column=0, padx=5, pady=5, sticky=tk.W)
 
-    noodvenstergeslachtlabel = tk.Label(master=noodmenuframe, background="royal blue", text="Geslacht: \t\t"+ databasereader("""SELECT geslacht from persoon where noodpersoonid = 1"""))
+    noodvenstergeslachtlabel = tk.Label(master=noodmenuframe1, background="royal blue", text="Geslacht: \t\t"+ databasereader("""SELECT geslacht from persoon where noodpersoonid = 1"""))
     noodvenstergeslachtlabel.grid(row=5, column=0, padx=5, pady=5, sticky=tk.W)
 
-    noodvenstergeboortedatumlabel = tk.Label(master=noodmenuframe, background="royal blue", text="Geboortedatum: \t\t"+ str(databasereader("""SELECT geboortedatum from persoon where noodpersoonid = 1""")))
+    noodvenstergeboortedatumlabel = tk.Label(master=noodmenuframe1, background="royal blue", text="Geboortedatum: \t\t"+ str(databasereader("""SELECT geboortedatum from persoon where noodpersoonid = 1""")))
     noodvenstergeboortedatumlabel.grid(row=6, column=0, padx=5, pady=5 ,sticky=tk.W)
 
-    noodvenstertelefoonnummerlabel = tk.Label(master=noodmenuframe, background="royal blue", text="Telefoonnummer: \t"+ databasereader("""SELECT telefoonnummer from persoon where noodpersoonid = 1"""))
+    noodvenstertelefoonnummerlabel = tk.Label(master=noodmenuframe1, background="royal blue", text="Telefoonnummer: \t"+ databasereader("""SELECT telefoonnummer from persoon where noodpersoonid = 1"""))
     noodvenstertelefoonnummerlabel.grid(row=7, column=0, padx=5, pady=5, sticky=tk.W)
 
-    noodvensterplaatsnaamlabel = tk.Label(master=noodmenuframe, background="royal blue", text="Plaatsnaam: \t\t" + databasereader("""SELECT plaatsnaam from persoon where noodpersoonid = 1"""))
+    noodvensterplaatsnaamlabel = tk.Label(master=noodmenuframe1, background="royal blue", text="Plaatsnaam: \t\t" + databasereader("""SELECT plaatsnaam from persoon where noodpersoonid = 1"""))
     noodvensterplaatsnaamlabel.grid(row=8, column=0, padx=5, pady=5, sticky=tk.W)
 
-    noodvensterhuisnummerlabel = tk.Label(master=noodmenuframe, background="royal blue", text="Huisnummer: \t\t" + databasereader("""SELECT huisnummer from persoon where noodpersoonid = 1"""))
+    noodvensterhuisnummerlabel = tk.Label(master=noodmenuframe1, background="royal blue", text="Huisnummer: \t\t" + databasereader("""SELECT huisnummer from persoon where noodpersoonid = 1"""))
     noodvensterhuisnummerlabel.grid(row=9, column=0, padx=5, pady=5, sticky=tk.W)
 
-    noodvensterpostcodelabel = tk.Label(master=noodmenuframe, background="royal blue", text="Postcode: \t\t" + databasereader("""SELECT postcode from persoon where noodpersoonid = 1"""))
+    noodvensterpostcodelabel = tk.Label(master=noodmenuframe1, background="royal blue", text="Postcode: \t\t" + databasereader("""SELECT postcode from persoon where noodpersoonid = 1"""))
+    noodvensterpostcodelabel.grid(row=10, column=0, padx=5, pady=5, sticky=tk.W)
+
+def noodvensterkamer2():
+    hoofdmenuterugknop = tk.Button(master=noodmenuframe2, width=5, text="Terug", command=toonhoofdmenuframe)
+    hoofdmenuterugknop.grid(row=11, column=0)
+
+    noodvenstercamerabeeldentabel = tk.Label(master=noodmenuframe2, background="royal blue", text="Camerabeelden: ")
+    noodvenstercamerabeeldentabel.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+
+    noodvenstercameralicht = tk.Label(master=noodmenuframe2, background="red", width=2)
+    noodvenstercameralicht.grid(row=0, column=1)
+
+    noodvenstercameraknop = tk.Button(master=noodmenuframe2, width=2)
+    noodvenstercameraknop.grid(row=0, column=2, padx=5, pady=5)
+
+    noodvenstercontactgegevenslabel = tk.Label(master=noodmenuframe2, background="royal blue",
+                                               text="Noodcontactgegevens", font=("", 15))
+    noodvenstercontactgegevenslabel.grid(row=1, column=0, padx=5, pady=5)
+
+    noodvenstervoornaamlabel = tk.Label(master=noodmenuframe2, background="royal blue", text="Voornaam: \t\t" + databasereader("""SELECT voornaam from persoon where noodpersoonid = 2"""))
+    noodvenstervoornaamlabel.grid(row=2, column=0, padx=5, pady=5, sticky=tk.W)
+
+    noodvenstertussenvoegsellabel = tk.Label(master=noodmenuframe2, background="royal blue",text="Tussenvoegsel: \t\t" + str(databasereader("""SELECT tussenvoegsel from persoon where noodpersoonid = 2""")))
+    noodvenstertussenvoegsellabel.grid(row=3, column=0, padx=5, pady=5, sticky=tk.W)
+
+    noodvensterachternaamlabel = tk.Label(master=noodmenuframe2, background="royal blue",text="Achternaam: \t\t" + databasereader("""SELECT achternaam from persoon where noodpersoonid = 2"""))
+    noodvensterachternaamlabel.grid(row=4, column=0, padx=5, pady=5, sticky=tk.W)
+
+    noodvenstergeslachtlabel = tk.Label(master=noodmenuframe2, background="royal blue",text="Geslacht: \t\t" + databasereader("""SELECT geslacht from persoon where noodpersoonid = 2"""))
+    noodvenstergeslachtlabel.grid(row=5, column=0, padx=5, pady=5, sticky=tk.W)
+
+    noodvenstergeboortedatumlabel = tk.Label(master=noodmenuframe2, background="royal blue",text="Geboortedatum: \t\t" + str(databasereader("""SELECT geboortedatum from persoon where noodpersoonid = 2""")))
+    noodvenstergeboortedatumlabel.grid(row=6, column=0, padx=5, pady=5, sticky=tk.W)
+
+    noodvenstertelefoonnummerlabel = tk.Label(master=noodmenuframe2, background="royal blue",text="Telefoonnummer: \t" + databasereader("""SELECT telefoonnummer from persoon where noodpersoonid = 2"""))
+    noodvenstertelefoonnummerlabel.grid(row=7, column=0, padx=5, pady=5, sticky=tk.W)
+
+    noodvensterplaatsnaamlabel = tk.Label(master=noodmenuframe2, background="royal blue",text="Plaatsnaam: \t\t" + databasereader("""SELECT plaatsnaam from persoon where noodpersoonid = 2"""))
+    noodvensterplaatsnaamlabel.grid(row=8, column=0, padx=5, pady=5, sticky=tk.W)
+
+    noodvensterhuisnummerlabel = tk.Label(master=noodmenuframe2, background="royal blue",text="Huisnummer: \t\t" + databasereader("""SELECT huisnummer from persoon where noodpersoonid = 2"""))
+    noodvensterhuisnummerlabel.grid(row=9, column=0, padx=5, pady=5, sticky=tk.W)
+
+    noodvensterpostcodelabel = tk.Label(master=noodmenuframe2, background="royal blue",text="Postcode: \t\t" + databasereader("""SELECT postcode from persoon where noodpersoonid = 2"""))
     noodvensterpostcodelabel.grid(row=10, column=0, padx=5, pady=5, sticky=tk.W)
 
 def toonhoofdmenuframe():
     hoofdmenu()
-    noodmenuframe.pack_forget()
+    noodmenuframe1.pack_forget()
+    noodmenuframe2.pack_forget()
     hoofdmenuframe.pack()
 
-def toonnoodmenuframe():
-    noodvenster()
+
+def toonnoodmenuframe1():
+    noodvensterkamer1()
     hoofdmenuframe.pack_forget()
-    noodmenuframe.pack()
+    toonnoodmenuframe1()
+
+def toonnoodmenuframe2():
+    noodvensterkamer2()
+    hoofdmenuframe.pack_forget()
+    noodmenuframe2.pack()
 
 def frames():
     global hoofdmenuframe
-    global noodmenuframe
+    global noodmenuframe1
+    global noodmenuframe2
     hoofdmenuframe = tk.Frame(root)
     hoofdmenuframe.configure(background="royal blue")
     hoofdmenuframe.pack()
 
-    noodmenuframe = tk.Frame(root)
-    noodmenuframe.configure(background="royal blue")
-    noodmenuframe.pack()
+    noodmenuframe1 = tk.Frame(root)
+    noodmenuframe1.configure(background="royal blue")
+    noodmenuframe1.pack()
 
+    noodmenuframe2 = tk.Frame(root)
+    noodmenuframe2.configure(background="royal blue")
+    noodmenuframe2.pack()
+
+#database reader
 def databasereader(x):
     try:
         conn = psycopg2.connect("dbname='idp_domotica' user='idpgroep' host='37.97.193.131' password='S67asbiMQA'")
